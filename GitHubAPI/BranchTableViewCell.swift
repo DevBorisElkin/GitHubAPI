02f4aca241e05b826b7e766aafe1738905213ae0
DevBorisElkin <ea.foreverrr@gmail.com>
2022-06-26 23:40:46 +0300

    backup come changes

---
+++ GitHubAPI/BranchTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  BranchTableViewCell.swift
+//  GitHubAPI
+//
+//  Created by test on 26.06.2022.
+//
+
+import UIKit
+
+class BranchTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var branchNameLabel: UILabel!
+    
+}
--- GitHubAPI/ViewController.swift
+++ GitHubAPI/ViewController.swift
@@ -6,17 +6,67 @@
 //
 
 import UIKit
+import RxSwift
 
 // check out
 // https://docs.github.com/en/rest
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    private let githubRepository = GitHubRepository()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        githubRepository.getRepos()
+        
     }
 
 
+}
+
+struct Repo: Decodable{
+    let name: String
+}
+
+struct Owner: Decodable{
+    let login: String
+}
+
+struct Branch: Decodable{
+    let name: String
+}
+
+class GitHubRepository{
+    private let networkService = NetworkService()
+    private let baseUrlString = "https://api.github.com"
+    
+    func getRepos() -> Observable<[Repo]> {
+        return networkService.execute(url: URL(string: "\(baseUrlString)/repositories")!)
+    }
+    
+    func getBranches(ownerName: String, repoName: String) -> Observable<[Branch]> {
+        return networkService.execute(url: URL(string: "\(baseUrlString)/repos/\(ownerName)/\(repoName)/branches")!)
+    }
+}
+
+class NetworkService{
+    func execute<T : Decodable>(url: URL) -> Observable<T> {
+        return Observable.create { observer in
+            let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+                guard let data = data, let decoded = try? JSONDecoder().decode(T.self, from: data) else { return }
+                
+                observer.onNext(decoded)
+                observer.onCompleted()
+            }
+            
+            task.resume()
+            return Disposables.create {
+                task.cancel()
+            }
+            
+        }
+    }
 }
 
