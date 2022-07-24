0818f41f59d847b3c423ca3c09d47dd16cb1ee96
DevBorisElkin <ea.foreverrr@gmail.com>
2022-07-24 14:23:57 +0300

    backup before all messed up

--- GitHubAPI/Details/ViewController/RepoDetailsViewController.swift
+++ GitHubAPI/Details/ViewController/RepoDetailsViewController.swift
@@ -10,20 +10,41 @@ import WebKit
 
 class RepoDetailsViewController: UIViewController {
     
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     var selectedRepository: Repository?
     let webView = WKWebView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(webView)
+        view.backgroundColor = .red
+        //view.addSubview(webView)
         
         guard let selectedRepository = selectedRepository, let url = URL(string: selectedRepository.html_url) else {
             print("Something is wrong with selected repository or with url to that repository")
             return
         }
 
-        webView.load(URLRequest(url: url))
+        setIndicatorActiveState(state: true)
+        
+        DispatchQueue.global(qos: .background).async { [weak self, url] in
+            self?.webView.load(URLRequest(url: url))
+            
+            print("code went forward, delay 5")
+            sleep(5)
+            print("delay passed")
+            
+            guard let vc = self, let wv = self?.webView else{
+                print("error with references")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                vc.setIndicatorActiveState(state: false)
+                vc.view.addSubview(wv)
+            }
+        }
     }
     
     override func viewDidLayoutSubviews() {
@@ -33,5 +54,14 @@ class RepoDetailsViewController: UIViewController {
     
     public func setData(repository: Repository){
         selectedRepository = repository
+    }
+    
+    private func setIndicatorActiveState(state: Bool){
+        if(state){
+            activityIndicator.startAnimating()
+        }else{
+            activityIndicator.stopAnimating()
+        }
+        activityIndicator.isHidden = !state
     }
 }
