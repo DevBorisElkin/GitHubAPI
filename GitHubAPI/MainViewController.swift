b053e06d69dfdf774e2ee6f13a63cf5ca84dbb19
DevBorisElkin <ea.foreverrr@gmail.com>
2022-07-24 19:44:40 +0300

    simplest pagination

--- GitHubAPI/MainViewController.swift
+++ GitHubAPI/MainViewController.swift
@@ -17,19 +17,34 @@ class MainViewController: UIViewController {
     var repos: [Repository]?
     var selectedRow: Int?
     
+    let githubReposLink = "https://api.github.com/repositories"
+    let githubReposPaginationLink = "https://api.github.com/repositories?since="
+    
+    var screenHeight: CGFloat!
+    let screenHeightOffsetToPaginate = 0.25 // e.g. 0.2 = 20% of screen swiped = load next page
+    var contentSizeSubtraction: Double!
+    
+    var fetchingData: Bool = true
+    var lastRepoLoadedId: Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        loadData()
+        screenHeight = UIScreen.main.bounds.height
+        //contentSizeSubtraction = screenHeight * (1 - screenHeightOffsetToPaginate)
+        contentSizeSubtraction = view.frame.size.height + Double(100)
+        loadDataInitially()
     }
     
-    func loadData(){
-        NetworkingHelpers.decodeDataDetailed(from: "https://api.github.com/repositories", type: [Repository].self, printJSON: false) { [weak self] data in
-            
-            //print(data)
+    func loadDataInitially(){
+        NetworkingHelpers.decodeDataDetailed(from: githubReposLink, type: [Repository].self, printJSON: false) { [weak self] data in
             
             self?.repos = data
             self?.tableView.reloadData()
+            self?.fetchingData = false
+            self?.lastRepoLoadedId = data.last?.id
+            
+            print("Number of repos: \(data.count)")
         }
     }
     
@@ -58,5 +73,39 @@ extension MainViewController : UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.selectedRow = indexPath.row
         performSegue(withIdentifier: "showRepositoryDetails", sender: nil)
+    }
+}
+
+// MARK: pagination related code
+extension MainViewController : UIScrollViewDelegate{
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard fetchingData == false else { return }
+        
+        let position = scrollView.contentOffset.y
+        
+        if(position > tableView.contentSize.height - contentSizeSubtraction){
+            
+            print("fetch more data")
+            fetchMoreData()
+        }
+    }
+    
+    func fetchMoreData(){
+        guard fetchingData == false, let lastRepoLoadedId = self.lastRepoLoadedId else { print("unknown error"); return }
+        
+        print("Load more repos data")
+        fetchingData = true
+        
+        NetworkingHelpers.decodeDataDetailed(from: githubReposPaginationLink + "\(lastRepoLoadedId)", type: [Repository].self, printJSON: false) { [weak self, lastRepoLoadedId] data in
+            
+            self?.repos?.append(contentsOf: data)
+            self?.tableView.reloadData()
+            
+            self?.lastRepoLoadedId = data.last?.id
+            self?.fetchingData = false
+            
+            print("Now total repos count: \(self?.repos?.count)")
+        }
     }
 }
